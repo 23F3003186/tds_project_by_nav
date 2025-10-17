f1364989598d499de934b10982fa8cc54e19c289
23F3003186 <23f3003186@ds.study.iitm.ac.in>
2025-10-17 21:08:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ async def receive_task(req: TaskRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
